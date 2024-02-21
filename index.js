bff76bc11d8217e1ecac0fb9493fd45585a6c48c
Kaio1000 <160607314+Kaio1000@users.noreply.github.com>
2024-02-21 14:38:57 +0000

    endpoint get

--- index.js
+++ index.js
@@ -2,11 +2,26 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello, World')
+  res.send('Hello World')
 })
 
 app.get("/oi", function (req, res) {
     res.send("Olá Mundo!")
+})
+
+// Lista de Personagens
+const lista = ['Rick Sanchez', 'Morty Smith', 'Summer Smith']
+
+// Read All -> [GET] /item
+app.get('/item', function (req, res) {
+  res.send(lista)
+})
+
+// Read By ID -> [GET] /item/:id
+app.get('/item/:id', function (req, res) {
+  const id = req.params.id
+
+  res.send(id)
 })
 
 app.listen(3000)
