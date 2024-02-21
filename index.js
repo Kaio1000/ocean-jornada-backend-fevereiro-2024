a4a4b4a1d4e69aa28794bf2b7f5a04adda77a134
Kaio1000 <160607314+Kaio1000@users.noreply.github.com>
2024-02-21 15:08:27 +0000

    endpoit `[POST]` /item

--- index.js
+++ index.js
@@ -21,8 +21,27 @@ app.get('/item', function (req, res) {
 app.get('/item/:id', function (req, res) {
   const id = req.params.id
 
+  //acesso o item na lista baseado no ID recebido
   const item = lista[id]
+  //envio o item obtido como resposta HTTP
   res.send(item)
+})
+//Sinaliza que o corpo da requisição está em JSON
+app.use(express.json())
+
+//Create -> [POST] /item
+app.post('/item', function(req, res){
+  //extraimos o corpo da requisiçao 
+  const body = req.body
+
+  //pegamos o nome (string) que foi enviado dentro do corpo
+  const item = body.nome 
+  
+  //colocamos o nome dentro da lista de itens
+  lista.push(item)
+
+  //enviamos uma resposta de sucesso
+  res.send('Item adicionado com sucesso!')
 })
 
 app.listen(3000)
